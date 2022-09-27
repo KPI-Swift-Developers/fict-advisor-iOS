ff98132963824375708c090d61e392f14366d133
Jeytery <dimaostapchenko@gmail.com>
2022-09-27 21:42:07 +0300

    teachersVC: small code style fixes

--- fictadvisor-iOS/Modules/Teachers/TeachersViewController.swift
+++ fictadvisor-iOS/Modules/Teachers/TeachersViewController.swift
@@ -7,18 +7,15 @@
 
 import UIKit
 
-
 class TeachersViewController: UIViewController {
+    
     private let service: TeachersServiceTarget
     private var teachers = Teachers()
+    private let tableView = UITableView()
     
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        
-        return tableView
-    }()
+    private var isAppeared = false
     
-    init (service: TeachersServiceTarget) {
+    init(service: TeachersServiceTarget) {
         self.service = service
         super.init(nibName: nil, bundle: nil)
         configureTableView()
@@ -27,8 +24,10 @@ class TeachersViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        if isAppeared { return }
+        isAppeared = true
         
-        service.getTeachers(page: 0, completion: {[weak self] _teachers in
+        service.getTeachers(page: 0, completion: { [weak self] _teachers in
             self?.displayTeachers(_teachers)
         }, errorCompletition: nil)
     }
@@ -55,22 +54,33 @@ private extension TeachersViewController {
         tableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        tableView.register(TeachersTableViewCell.self, forCellReuseIdentifier: TeachersTableViewCell.identifier)
+        tableView.register(
+            TeachersTableViewCell.self,
+            forCellReuseIdentifier: TeachersTableViewCell.identifier)
         tableView.delegate = self
         tableView.dataSource = self
     }
 }
 
 extension TeachersViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
         return teachers.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: TeachersTableViewCell.identifier, for: indexPath) as? TeachersTableViewCell
-        guard let cell = cell else { return UITableViewCell()}
-        cell.nameLabel.text = teachers[indexPath.row].firstName + " " + teachers[indexPath.row].lastName
-        cell.ratingLabel.text = "Рейтинг: " + String(teachers[indexPath.row].rating)
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: TeachersTableViewCell.identifier,
+            for: indexPath) as! TeachersTableViewCell
+        let teacher = teachers[indexPath.row]
+        let nameString = teacher.firstName + " " + teacher.lastName
+        cell.nameLabel.text = nameString
+        cell.ratingLabel.text = "Рейтинг: " + String(teacher.rating)
         return cell
     }
 }
