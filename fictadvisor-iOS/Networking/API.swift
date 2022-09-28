222de9fd1f8291168e8f13aba3ffa906cdaa7b29
Jeytery <dimaostapchenko@gmail.com>
2022-09-28 13:00:15 +0300

    api changes (add sorting type)

--- fictadvisor-iOS/Networking/API.swift
+++ fictadvisor-iOS/Networking/API.swift
@@ -16,3 +16,10 @@ struct APIArrayData<Data: Codable>: Codable {
     let count: Int
     let items: [Data]
 }
+
+struct SortingType {
+    let urlName: String
+    
+    static let byName = SortingType(urlName: "name")
+    static let byRate = SortingType(urlName: "rate")
+}
